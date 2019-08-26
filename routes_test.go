436bb839360e548f04a4ab28d50d95f8bf8f31a5
Nathan Webb <nwebb@ricoh.com.au>
2019-08-26 17:17:09 +1000

    local commands are working

--- routes_test.go
+++ routes_test.go
@@ -104,13 +104,28 @@ func TestGetResults(t *testing.T) {
 	r := gin.Default()
 	r.GET(apiURL, getResults)
 
-	requests := []string{
-		"?name=local ping&ip=127.0.0.1",
-		"?name=local ping&ip=127.0.0.1;touch test.txt",
+	tests := []struct {
+		request    string
+		statusCode int
+	}{
+		{"?name=local ping&ip=127.0.0.1", http.StatusOK},
+		{"?name=local ping&ip=127.0.0.1;touch malicious_test.txt", http.StatusOK},
+		{"?name=cat&ip=/etc/passwd", http.StatusBadRequest},
 	}
-	for _, request := range requests {
-		req, _ := http.NewRequest("GET", apiURL+request, nil)
+	for _, c := range tests {
+		req, _ := http.NewRequest("GET", apiURL+c.request, nil)
 		testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+			_, err := os.Open("malicious_test.txt")
+			if err == nil || !os.IsNotExist(err) {
+				t.Errorf("malicious attack was successful")
+			}
+			statusOK := w.Code == c.statusCode
+			if !statusOK {
+				t.Errorf("expected status code: %d, got %d", c.statusCode, w.Code)
+			}
+			if c.statusCode != http.StatusOK {
+				return statusOK
+			}
 			p, err := ioutil.ReadAll(w.Body)
 			if err != nil {
 				t.Errorf("%s", err.Error())
@@ -121,10 +136,9 @@ func TestGetResults(t *testing.T) {
 				t.Error(err.Error())
 			}
 			if result.Stdout == "" {
-				t.Errorf("no output returned")
+				t.Errorf("no output returned: %s", result.Stdout)
 			}
 
-			statusOK := w.Code == http.StatusOK
 			return statusOK
 		})
 	}
